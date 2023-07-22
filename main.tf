13a98f2845b335ba177b4f7532fc0dae7e297af0
sudhakar-avula <39494569+sudhakar-avula@users.noreply.github.com>
2023-07-22 17:55:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
-  region = var.aws_region
+  region = 'us-east-1'
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
+    bucket = "s3-asr-terraform-first-project"
+    acl = "private"
+
+    tags = {
+        Name = "My bucket"
+    }
 }
