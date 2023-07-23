3688a14362ae50157118be38fcb8544140a1431d
sudhakar-avula <39494569+sudhakar-avula@users.noreply.github.com>
2023-07-23 13:55:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,14 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my-s3-bucket" {
     bucket = "s3-asr-terraform-first-project-023611"
-    acl = "private"
 
     tags = {
         Name = "My bucket"
     }
+}
+
+resource "aws_s3_bucket_acl" "my-s3-bucket-acl" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+
+  acl = "private"
 }
