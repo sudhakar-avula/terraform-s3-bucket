44c761cfd16f27af4bd5e0ab4e0c567e592e72af
sudhakar-avula <39494569+sudhakar-avula@users.noreply.github.com>
2023-07-23 17:10:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,23 +3,19 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
-    bucket = "s3-asr-terraform-first-project-023611"
+  bucket = "s3-asr-terraform-first-project-023611"
+}
 
-    tags = {
-        Name = "My bucket"
-    }
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "my-s3-bucket-acl" {
-  bucket = aws_s3_bucket.my-s3-bucket.id
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 
-  acl = "private"
-}
-
-# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.my-s3-bucket.id
-  rule {
-    object_ownership = "ObjectWriter"
-  }
+  acl    = "private"
 }
