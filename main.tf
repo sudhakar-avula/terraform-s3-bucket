5edb6edbc6c68377e0f0ad26c45bfb472da83b65
sudhakar-avula <39494569+sudhakar-avula@users.noreply.github.com>
2023-07-23 14:07:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
-}
-
 provider "aws" {
-  region = "us-east-1a"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
