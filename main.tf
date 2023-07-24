bc31fa378db99689bd939a68e375af3797bccece
Sudhakar Reddy Avula <39494569+sudhakar-avula@users.noreply.github.com>
2023-07-23 22:14:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket = "s3-asr-terraform-first-project-023611"
+  bucket = "s3-asr-terraform-first-project-023612"
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
